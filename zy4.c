87f2fc622f8c1d441331d60c38235aa1223d6afe
SakuraNeko <NekoCN@outlook.com>
2016-11-01 10:43:29 +0800

    Revert "SakuraNeko"
    This reverts commit 90feaa464defb1a73c61eaec622c8f57a0d0a0df.

--- zy4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a,b,c,d,e;
-	while(scanf("%d %d %d %d %d",&a,&b,&c,&d,&e) != EOF)
-	{
-		printf("%d\n",100-a-b-c-d-e);
-	}
-}
--- zy8.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-	double r,s,pi;
-	while (scanf("%lf",&r)!=EOF){	
-		pi = 3.1415;
-		s = pi*r*r;
-		printf("%.4lf\n",s);
-	}
-}
