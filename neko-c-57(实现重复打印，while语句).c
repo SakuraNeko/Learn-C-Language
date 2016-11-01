39276b26270d0fd2892fd8d59c915ec7569e76cc
SakuraNeko <NekoCN@outlook.com>
2016-11-01 10:44:44 +0800

    Revert "SakuraNeko"
    This reverts commit 22b06f87723898769e337e35cfe696e9f7e9d978.

--- neko-c-57(实现重复打印，while语句).c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int i;i=0;
-	while(i<6){
-		printf("****************\n Happy New Year \n****************\n");
-		i=i+1;
-	}	
-}
--- neko-c-57-2(实现重复打印，for语句).c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	int i;
-	for(i=1;i<6;i=i+1){
-		printf("****************\n Happy New Year \n****************\n");
-	}	
-}
